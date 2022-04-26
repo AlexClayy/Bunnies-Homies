cc9e5ab90aeab917469fbe6dddf5bfa4becdede4
wdserverr <42107311+wdserverr@users.noreply.github.com>
2022-04-26 15:10:14 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,14 @@ import { HomeView } from "../views";
 const Home: NextPage = (props) => {
   return (
     <div>
+            <meta property="og:type" content="website"/>
+            <meta property="og:site_name" content="Bunnies N Homies"/>
+            <meta property="og:title" content="Mint Here"/>
+            <meta property="og:description" content="A collection of the chillest BUNNIES on the block(chain)in search of immaculate vibes.  They’re bringing the party to Solana, and you’re invited."/>
+            <meta property="og:image" content="https://pbs.twimg.com/profile_images/1513637794953977861/ZPQyAZZC_400x400.jpg"/>
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:site" content="@discord"/>
+            <meta name="twitter:creator" content="@yourtwitter"/>
       <Head>
       <title>Bunnies N Homies - MINT</title>
         <meta

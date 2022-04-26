b0214495b67e51ebc615523d01ae1bd9b729eb47
wdserverr <42107311+wdserverr@users.noreply.github.com>
2022-04-26 15:15:56 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,21 @@ const Home: NextPage = (props) => {
   return (
     <div>
             <meta property="og:type" content="website"/>
-            <meta property="og:site_name" content="Bunnies N Homies"/>
+            <meta name="author" content="John Doe">
+            <meta property="og:site_name" content="Boss Bunny"/>
             <meta property="og:title" content="Mint Here"/>
-            <meta property="og:description" content="A collection of the chillest BUNNIES on the block(chain)in search of immaculate vibes.  They’re bringing the party to Solana, and you’re invited."/>
+            <meta property="og:description" content="A collection of the chillest BUNNIES on the block(chain)in search of immaculate vibes.  They’re bringing the party to Solana, and you’re invited.
+                                                     Public
+                                                      Sale
+
+                                                      Supply
+                                                      888
+
+                                                      Max
+                                                      2
+
+                                                      Price
+                                                      1 Sol"/>
             <meta property="og:image" content="https://pbs.twimg.com/profile_images/1513637794953977861/ZPQyAZZC_400x400.jpg"/>
             <meta name="twitter:card" content="summary_large_image"/>
             <meta name="twitter:site" content="@discord"/>
